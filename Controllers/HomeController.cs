6e78f7c5164cfed0ab21589eba55ccb8c950a291
Sơn Nguyễn <61671615+sonbill@users.noreply.github.com>
2023-06-28 16:27:14 +0700

    Update Display and Async

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -123,10 +123,12 @@ namespace feast_mansion_project.Controllers
         [HttpPost]
         public async Task<IActionResult> UpdateProfile(ProfileViewModel model)
         {
-            if (!int.TryParse(HttpContext.Session.GetString("UserId"), out int userId))
+            if (HttpContext.Session.GetString("userId") == null)
             {
                 return RedirectToAction("Index", "Home");
             }
+
+            int userId = Convert.ToInt32(HttpContext.Session.GetString("userId"));
 
             // Retrieve the existing customer record from the database
             var customer = await _dbContext.Customers.FindAsync(userId);
@@ -163,7 +165,7 @@ namespace feast_mansion_project.Controllers
 
 
         [HttpGet("ChangePassword")]
-        public async Task<IActionResult> ChangePassword()
+        public IActionResult ChangePassword()
         {
             if (HttpContext.Session.GetString("userId") == null)
             {
@@ -183,31 +185,35 @@ namespace feast_mansion_project.Controllers
 
             int userId = Convert.ToInt32(HttpContext.Session.GetString("userId"));
 
-            var orders = _dbContext.Orders
+            //var orders = await _dbContext.Orders
+            //    .Include(o => o.OrderDetails)
+            //    .ThenInclude(od => od.Product)
+            //    .Where(o => o.CustomerId == userId)
+            //    .OrderByDescending(o => o.OrderDate)
+            //    .ToListAsync();
+
+            var orders = await _dbContext.Orders
                 .Include(o => o.OrderDetails)
                 .ThenInclude(od => od.Product)
                 .Where(o => o.CustomerId == userId)
-                .OrderByDescending(o => o.OrderDate);
+                .OrderByDescending(o => o.OrderDate)
+                .ToListAsync();
 
-            int totalItems = await orders.CountAsync();
+            int totalItems = orders.Count;
 
             int totalPages = (int)Math.Ceiling((double)totalItems / pageSize);
 
-            var paginatedOrders = await orders.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
+            var paginatedOrders = orders.Skip((page - 1) * pageSize).Take(pageSize).ToList();
 
             OrderHistoryViewModel orderHistoryViewModel = new OrderHistoryViewModel
             {
                 Orders = paginatedOrders,
-
                 TotalItems = totalItems,
-
                 CurrentPage = page,
-
                 PageSize = pageSize,
-
                 TotalPages = totalPages
             };
-            //var order = _dbContext.Orders.Where(o => o.CustomerId == userId).ToList();
+
             return View("OrdersHistoryList", orderHistoryViewModel);
         }
 
